f1cb82ab3f678cd24dce2947f9c21ee5fe499e3c
pooja gupta <pooja956078@gmail.com>
2026-01-06 17:49:22 +0530

    Add files via upload

---
+++ kTech/assets/js/main.js
@@ -0,0 +1,48 @@
+﻿let tasks = [];
+let editIndex = null;
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let value = input.value.trim();
+
+    if (value === "") return;
+
+    if (editIndex !== null) {
+        tasks[editIndex] = value; // UPDATE
+        editIndex = null;
+    } else {
+        tasks.push(value);
+    }
+
+    input.value = "";
+    displayTasks();
+}
+
+// READ
+function displayTasks() {
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        list.innerHTML += `
+      <li>
+       <span class="taskListadd">
+        ${task}
+        <button onclick="editTask(${index})">Edit</button>
+        <button onclick="deleteTask(${index})">Delete</button>
+       </span>
+      </li>
+    `;
+    });
+}
+
+// UPDATE
+function editTask(index) {
+    document.getElementById("taskInput").value = tasks[index];
+    editIndex = index;
+}
+
+// DELETE
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    displayTasks();
+}
